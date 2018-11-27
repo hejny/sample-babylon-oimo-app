c74a3bed0315c3681c8d1bb4114d51518f2457ee
Pavol Hejny <me@pavolhejny.com>
2018-11-27 15:45:07 +0100

    cleanup

--- src/createMaterial.ts
+++ src/createMaterial.ts
@@ -1,15 +1,6 @@
 import * as BABYLON from 'babylonjs';
 
-/*sphere.material = new BABYLON.StandardMaterial('material',scene);
-    sphere.material.backFaceCulling = false;
-    sphere.material.emissiveTexture = new BABYLON.Texture(PLANET, scene); 
-    sphere.material.emissiveTexture.uScale = -1; 
-    sphere.material.emissiveTexture.vScale = -1;*/
-
-
-
 export function createMaterialColor(colorHex: string,scene: BABYLON.Scene):BABYLON.Material{
-    
     const material = new BABYLON.StandardMaterial('material',scene);
     material.diffuseColor = BABYLON.Color3.FromHexString(colorHex);
     return material;
